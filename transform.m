23389da41dbbe761cc39891f7d41eae0bd06d336
Yue Liu <liuyue002@gmail.com>
2020-12-07 15:19:11 +0000

    generalize figure and domain size

--- transform.m
+++ transform.m
@@ -1,17 +1,22 @@
-function newIm = transform (A, f, L)
+function newIm = transform (A, f, L, imgsize, domainpixel)
+%Transform the image with a complex function
+% A: original image, must be square
+% f: the complex map (x,y) -> f(x+iy)
+% L: domain size [-L,L]^2
+% imgsize: the original image tiles [0,imgsize]^2, periodically
+% domainpixel: how many pixels the whole transformed image have
 
-% original image represent x, y in [0,1]
-%f=@(x,y) cos(x+1i*y);
-dx=1/(size(A,1)-1);
-nx=2*L/dx+1;
+Asize = size(A,1);
+nx=domainpixel;
+dx=2*L/(nx-1);
 x=-L:dx:L;
 y=-L:dx:L;
 [X,Y]=meshgrid(x,y);
 newIm = zeros(nx,nx,3,'uint8');
 
 transf = f(X,Y);
-R = min(round(mod(real(transf),1)*(1/dx))+1,(1/dx)+1);
-I = min(round(mod(imag(transf),1)*(1/dx))+1,(1/dx)+1);
+R = min(round(mod(real(transf),imgsize)*(Asize/imgsize))+1,Asize);
+I = min(round(mod(imag(transf),imgsize)*(Asize/imgsize))+1,Asize);
 
 for layer = 1:3
     ind = sub2ind(size(A),R,I,layer*ones(size(R)));
